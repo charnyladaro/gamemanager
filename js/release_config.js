f52593aa388d1c035b95500883f063ddb5df1fd9
Charnyl Adaro <39955916+charnyladaro@users.noreply.github.com>
2025-11-26 13:17:14 +0800

    Change download URL in release configuration
    Updated download URL to point to the new executable file.

--- js/release_config.js
+++ js/release_config.js
@@ -4,9 +4,9 @@ const releaseConfig = {
 
     // The URL where the download button should point to
     // You can use the direct link to the .exe file on GitHub releases
-    // Example: https://github.com/charnyladaro/gamemanager/releases/download/GameManager/GameManager-Setup.exe
+    // Example: https://github.com/charnyladaro/gamemanager/releases/download/GameManager/GameManager.Setup.1.0.0.exe
     // Or just the release page: https://github.com/charnyladaro/gamemanager/releases/tag/GameManager
-    downloadUrl: "https://github.com/charnyladaro/gamemanager/releases/tag/GameManager",
+    downloadUrl: "https://github.com/charnyladaro/gamemanager/releases/download/GameManager/GameManager.Setup.1.0.0.exe",
 
     // The text to display below the hero section title
     heroNote: "Version 1.0.0 • Windows 10/11 • Free Forever",
@@ -14,6 +14,7 @@ const releaseConfig = {
     // The text to display below the download button
     downloadNote: "Version 1.0.0 • 169 MB"
 };
+
 
 
 
