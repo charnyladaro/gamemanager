be9518d0469b34a99cebd64a092adca6b4ea896c
Charnyl Adaro <39955916+charnyladaro@users.noreply.github.com>
2025-11-26 12:56:32 +0800

    Fix download URL format in release_config.js

--- js/release_config.js
+++ js/release_config.js
@@ -6,7 +6,8 @@ const releaseConfig = {
     // You can use the direct link to the .exe file on GitHub releases
     // Example: https://github.com/charnyladaro/gamemanager/releases/download/GameManager/GameManager-Setup.exe
     // Or just the release page: https://github.com/charnyladaro/gamemanager/releases/tag/GameManager
-    downloadUrl: "https://github.com//charnyladaro/gamemanager/releases/download/GameManager/GameManager.exe",
+    downloadUrl: "https://github.com//charnyladaro/gamemanager/releases/download/GameManager/GameManager.Setup.1.0.0.exe
+",
 
     // The text to display below the hero section title
     heroNote: "Version 1.0.0 • Windows 10/11 • Free Forever",
@@ -14,3 +15,4 @@ const releaseConfig = {
     // The text to display below the download button
     downloadNote: "Version 1.0.0 • 180 MB"
 };
+
