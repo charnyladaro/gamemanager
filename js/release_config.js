e9fb8ded16be0b0d6f6b04bcf83773f8f5f962a6
Charnyl Adaro <39955916+charnyladaro@users.noreply.github.com>
2025-11-26 13:02:41 +0800

    Fix download note file size in release config
    Updated the download note to reflect the correct file size.

--- js/release_config.js
+++ js/release_config.js
@@ -6,13 +6,13 @@ const releaseConfig = {
     // You can use the direct link to the .exe file on GitHub releases
     // Example: https://github.com/charnyladaro/gamemanager/releases/download/GameManager/GameManager-Setup.exe
     // Or just the release page: https://github.com/charnyladaro/gamemanager/releases/tag/GameManager
-    downloadUrl: "https://github.com//charnyladaro/gamemanager/releases/download/GameManager/GameManager.Setup.1.0.0.exe
-",
+    downloadUrl: "https://github.com/charnyladaro/gamemanager/releases/download/GameManager/GameManager.Setup.1.0.0.exe",
 
     // The text to display below the hero section title
     heroNote: "Version 1.0.0 • Windows 10/11 • Free Forever",
 
     // The text to display below the download button
-    downloadNote: "Version 1.0.0 • 180 MB"
+    downloadNote: "Version 1.0.0 • 169 MB"
 };
+
 
